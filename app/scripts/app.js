fd8c6e1dbbd87228655db8f59c2f276075e778de
Nathan Kluth <nlkluth@gmail.com>
2014-02-07 09:48:11 -0700

    adding routes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,6 +12,12 @@ angular.module('versed', [
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/profile', {
+        templateUrl: 'views/profile.html'
+      })
+      .when('/library', {
+        templateUrl: 'views/library.html'
+      })
       .otherwise({
         redirectTo: '/'
       });
