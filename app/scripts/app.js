f269e2ef368ba2bade7e07304b92ec37426f26bd
Nathan Kluth <nlkluth@gmail.com>
2014-02-07 22:45:54 -0700

    footer navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,18 +9,18 @@ var app = angular.module('versed', [
 ]);
 
 app.config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/profile', {
-        templateUrl: 'views/profile.html'
-      })
-      .when('/library', {
-        templateUrl: 'views/library.html'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/profile', {
+      templateUrl: 'views/profile.html'
+    })
+    .when('/library', {
+      templateUrl: 'views/library.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
