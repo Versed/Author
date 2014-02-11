dae5a04d8e7f13a03e4f04a10e111f1731d47824
Nathan Kluth <nlkluth@gmail.com>
2014-02-11 12:03:46 -0700

    adding dependencies

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var controllers = angular.module('versed.main', []);
+var controllers = angular.module('versed.main', ['versed.bookService']);
 
 controllers.controller('MainCtrl', ['$scope', 'Books', function ($scope, Books) {
   $scope.books = Books.get();
--- app/scripts/service/books.js
+++ app/scripts/service/books.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var services = angular.module('versed.bookService', []);
+var services = angular.module('versed.bookService', ['ngResource']);
 
 services.factory('Books', ['$resource', function($resource) {
   return $resource('books.json', {id: '@id'});
