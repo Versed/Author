9a3e6ff2cd3fe8a9b0ab540387d221dd2e644ee3
Nathan Kluth <nlkluth@gmail.com>
2014-02-12 10:07:46 -0700

    pulled config into separate file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,40 +5,10 @@ var app = angular.module('versed', [
   'ngResource',
   'ngSanitize',
   'ui.router',
+  'versed.config',
   'versed.main',
   'versed.books',
   'versed.lessons',
   'versed.error',
   'versed.bookService'
 ]);
-
-app.config(['$stateProvider', '$urlRouterProvider',
- function ($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('main', {
-      url: '/',
-      templateUrl: 'views/main.html',
-      controller: 'MainCtrl'
-    })
-    .state('books', {
-      url: '/books',
-      templateUrl: 'views/books.html',
-      controller: 'BookCtrl'
-    })
-    .state('profile', {
-      url: '/profile',
-      templateUrl: 'views/profile.html'
-    })
-    .state('lessons', {
-      url: '/lessons',
-      templateUrl: 'views/lesson.html',
-      controller: 'LessonCtrl'
-    })
-    .state('404', {
-      url: '/404',
-      templateUrl: '404.html'
-    });
-
-  $urlRouterProvider.otherwise('/404');
-  // $locationProvider.html5Mode(true).hashPrefix('!');
-}]);
