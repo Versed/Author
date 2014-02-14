5e3fb2369c45b8eb1a1b6ac0bc4cfb7da2c9ecdb
Nathan Kluth <nlkluth@gmail.com>
2014-02-13 17:42:35 -0700

    debugging tests...

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 describe('Controllers', function () {
-
+  var mockBooksResource, $httpBackend;
   beforeEach(module('versed'));
 
   it('should be true', function() {
@@ -9,28 +9,44 @@ describe('Controllers', function () {
   });
 
   describe('MainController', function() {
-    var MainController, scope, Books, $save;
+    // var MainController, scope, Books, $save;
 
-    beforeEach(inject(function ($controller, $rootScope) {
-      scope = $rootScope.$new();
-      $save = jasmine.createSpy('$save');
-      Books = function Books() {
-        return {
-          $save: $save
-        };
-      };
-      Books.get = function get() {
-        return [
+    beforeEach(function() {
+      angular.mock.inject(function($injector) {
+        $httpBackend = $injector.get($httpBackend);
+        mockBooksResource = $injector.get('Books');
+      });
+    });
+
+    // beforeEach(inject(function ($controller, $rootScope) {
+    //   scope = $rootScope.$new();
+    //   $save = jasmine.createSpy('$save');
+    //   Books = function Books() {
+    //     return {
+    //       $save: $save
+    //     };
+    //   };
+    //   Books.get = function get() {
+    //     return [
+    //       {name: 'Books 1', difficulty: 'beginner'},
+    //       {name: 'Books 2', difficulty: 'beginner'},
+    //       {name: 'Books 3', difficulty: 'beginner'}
+    //     ];
+    //   };
+    //   MainController = $controller('MainController', { $scope: scope, books: Books });
+    // }));
+
+    it('should populate scope with list of books', function() {
+      $httpBackend.expectGET('books.json')
+        .respond([
           {name: 'Books 1', difficulty: 'beginner'},
           {name: 'Books 2', difficulty: 'beginner'},
           {name: 'Books 3', difficulty: 'beginner'}
-        ];
-      };
-      MainController = $controller('MainController', { $scope: scope, books: Books });
-    }));
+        ]);
 
-    it('should populate scope with list of books', function() {
-      expect(scope.books.length).toEqual(3);
+      var result = mockBooksResource.get();
+      $httpBackend.flush();
+      expect(result.length).toEqual(3);
     });
   });
 });
