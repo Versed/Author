384283ab95ea249ca8b0bf23ee2538ffe04c6fd2
Nathan Kluth <nlkluth@gmail.com>
2014-02-17 22:50:41 -0700

    updated mock - working test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -1,37 +1,35 @@
 'use strict';
 
 describe('Controllers', function () {
-  var MainController;
   beforeEach(module('versed'));
 
-  it('should be true', function() {
-    expect(true).toEqual(true);
-  });
+  var MainController, scope, $save, Books;
 
   describe('MainController', function() {
-    var scope, Books, $save;
-
     beforeEach(inject(function ($controller, $rootScope) {
       scope = $rootScope.$new();
       $save = jasmine.createSpy('$save');
+
       Books = function Books() {
         return {
           $save: $save
         };
       };
-      Books.get = function get() {
+      Books.query = function query() {
         return [
           {name: 'Books 1', difficulty: 'beginner'},
           {name: 'Books 2', difficulty: 'beginner'},
           {name: 'Books 3', difficulty: 'beginner'}
         ];
       };
-      MainController = $controller('MainController', { $scope: scope, books: Books.get() });
+      MainController = $controller('MainController', {
+        $scope: scope,
+        Books: Books
+      });
     }));
 
     it('should populate scope with list of books', function() {
-      var result = MainController.books;
-      expect(result.length).toEqual(3);
+      expect(scope.books.length).toBe(3);
     });
   });
 });
