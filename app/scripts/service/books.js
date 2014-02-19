939fc08052ec149f8b89d0c63824c1fb8885d38a
Nathan Kluth <nlkluth@gmail.com>
2014-02-19 13:58:22 -0700

    updated books location

--- app/scripts/service/books.js
+++ app/scripts/service/books.js
@@ -3,5 +3,5 @@
 var services = angular.module('versed.services.books', ['ngResource']);
 
 services.factory('Books', ['$resource', function($resource) {
-  return $resource('books.json', {id: '@id'});
+  return $resource('/lib/books.json', {});
 }]);
