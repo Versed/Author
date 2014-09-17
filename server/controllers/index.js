b9b896024c06c1715a909d4a13638d6ed79abe20
Nathan Kluth <nlkluth@gmail.com>
2014-09-17 09:35:50 -0600

    scratch

--- server/controllers/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.render = function(req, res) {
-
-  // Send some basic starting info to the view
-  res.render('index');
-};
--- server/routes.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function(app) {
-  var index = require('./controllers/index');
-
-  app.get('/', index.render);
-};
