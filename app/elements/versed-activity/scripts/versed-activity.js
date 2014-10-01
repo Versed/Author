ffd5b397457f3b3210d17b64cbeb7d1aef9a6163
Nathan Kluth <nlkluth@gmail.com>
2014-10-01 08:55:49 -0600

    core icon handles it

--- app/elements/versed-activity/scripts/versed-activity.js
+++ app/elements/versed-activity/scripts/versed-activity.js
@@ -1,18 +1,4 @@
 'use strict';
 
-var filters = {
-  'completion': '',
-  'levelUp': '',
-  'newSkill': '',
-  'generic': ''
-};
-
 Polymer({
-  filterType: function(value) {
-    if (filters[value]) {
-      return filters[value];
-    }
-
-    return filters.generic;
-  }
 });
